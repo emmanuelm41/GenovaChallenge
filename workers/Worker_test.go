098252a30506f72cc4bd1cc1555bb6a2970832ee
emmanuelm41 <emmanuelm41@gmail.com>
2020-08-01 20:04:00 -0300

    fix Workers test module

--- workers/Worker_test.go
+++ workers/Worker_test.go
@@ -1,11 +1,18 @@
 package workers
 
 import (
+	"GenovaChallenge/models"
+	"GenovaChallenge/satelites"
 	"testing"
 )
 
 func TestGetLocation(t *testing.T) {
-	_, _, _, err1 := getLocation(5.2, 8.6, 12.5)
+	sato := models.Sato{X: satelites.SatoPosX, Y: satelites.SatoPosY, Z: satelites.SatoPosZ}
+	kenobi := models.Kenobi{X: satelites.KenobiPosX, Y: satelites.KenobiPosY, Z: satelites.KenobiPosZ}
+	skywalker := models.Skywalker{X: satelites.SkywalkerPosX, Y: satelites.SkywalkerPosY, Z: satelites.SkywalkerPosZ}
+
+	w := Worker{Kenobi: kenobi, Sato: sato, Skywalker: skywalker}
+	_, _, _, err1 := w.GetLocation(5.2, 8.6, 12.5)
 
 	if err1 != nil {
 		t.Errorf("Error found trying to get position: %v \n", err1)
@@ -13,7 +20,12 @@ func TestGetLocation(t *testing.T) {
 }
 
 func TestGetMessage(t *testing.T) {
-	_, err2 := getMessage([]string{"This", "", "", "message"}, []string{"", "a", "new", "message"}, []string{"This", "", "", ""})
+	sato := models.Sato{X: satelites.SatoPosX, Y: satelites.SatoPosY, Z: satelites.SatoPosZ}
+	kenobi := models.Kenobi{X: satelites.KenobiPosX, Y: satelites.KenobiPosY, Z: satelites.KenobiPosZ}
+	skywalker := models.Skywalker{X: satelites.SkywalkerPosX, Y: satelites.SkywalkerPosY, Z: satelites.SkywalkerPosZ}
+
+	w := Worker{Kenobi: kenobi, Sato: sato, Skywalker: skywalker}
+	_, err2 := w.GetMessage([]string{"This", "", "", "message"}, []string{"", "a", "new", "message"}, []string{"This", "", "", ""})
 
 	if err2 != nil {
 		t.Errorf("Error found trying to get message: %v \n", err2)
